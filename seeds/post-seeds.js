33682d83e90a5450c15eb281d4b187aca1561d9d
Otafu <clinton.tarzia@gmail.com>
2022-05-04 19:46:41 -0400

    post-seeds

---
+++ seeds/post-seeds.js
@@ -0,0 +1,24 @@
+const { Post}=require("../models")
+
+const userData=[
+    {
+        title: 'Finals Highlights',
+        post_url: "https://www.youtube.com/watch?v=QEPjD3elq2M",
+        post_text: 'These are my highlights during the 2000 Finals',
+        user_id: 1
+    },
+    {
+        title: 'My Top Plays',
+        post_url: "https://www.youtube.com/watch?v=sgI0YCVse9U",
+        post_text: 'These are my career highlights',
+        user_id: 2
+    },
+    {
+        title: 'My best three-point contest',
+        post_url: "https://www.youtube.com/watch?v=Os3Z8CutS-Q",
+        post_text: 'My best score during a three-point contest',
+        user_id: 3
+    },
+];
+const seedPost=() => Post.bulkCreate(postData);
+module.exports=seedPost
