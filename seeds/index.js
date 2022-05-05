c586118595cf22f073897f405c104f96b105f878
Otafu <clinton.tarzia@gmail.com>
2022-05-04 20:24:17 -0400

    index.js

---
+++ seeds/index.js
@@ -0,0 +1,17 @@
+const seedUsers=require("./user-seeds");
+const seedPosts=require("./post-seeds");
+
+const sequelize=require("../config/connection")
+
+const seedAll=async () => {
+    await sequelize.sync({ force: true })
+
+    await seedUsers();
+    console.log ("Users Seeded")
+    await seedPosts();
+    console.log ("Users Seeded")
+
+    process.exit(0);
+}
+
+seedAll();
